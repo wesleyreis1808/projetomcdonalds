f1655fe76e721f43832d9270bec236ac5f357ee0
jeffersoncleyson <jeffersonalmeida@gec.inatel.br>
2017-11-09 16:46:05 -0200

    Teste de Busca de dados no banco OK

--- McDonalds/src/br/inatel/projeto/controler/Main.java
+++ McDonalds/src/br/inatel/projeto/controler/Main.java
@@ -3,16 +3,13 @@ package br.inatel.projeto.controler;
 
 import br.inatel.projeto.view.*;
 
-
 public class Main {
     
     public static void main(String[] args) {
         //Inicial inicial = new Inicial();
         //new ControlerInicial(inicial);
-        
         Login login = new Login();
         new ControlerLogin(login);
-        
         
     }
     
