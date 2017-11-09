c376daa5019b908ee13a106b2ae2205cacf40de0
jeffersoncleyson <jeffersonalmeida@gec.inatel.br>
2017-11-09 16:19:36 -0200

    Removendo comentario da Main

--- McDonalds/src/br/inatel/projeto/controler/Main.java
+++ McDonalds/src/br/inatel/projeto/controler/Main.java
@@ -1,17 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package br.inatel.projeto.controler;
 
 import br.inatel.projeto.view.*;
 
 
-/**
- *
- * @author WesleyReis
- */
 public class Main {
     
     public static void main(String[] args) {
