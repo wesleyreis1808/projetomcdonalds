a45850cf61d829b48f712fa9f094db0d6c703de8
Wesley Silva <wesleyreis@gec.inatel.br>
2017-11-15 09:54:43 -0200

    private.xml

--- McDonalds/src/br/inatel/projeto/controler/Main.java
+++ McDonalds/src/br/inatel/projeto/controler/Main.java
@@ -6,10 +6,10 @@ import br.inatel.projeto.view.*;
 public class Main {
     
     public static void main(String[] args) {
-        //Inicial inicial = new Inicial();
-        //new ControlerInicial(inicial);
-        Login login = new Login();
-        new ControlerLogin(login);
+        Inicial inicial = new Inicial();
+        new ControlerInicial(inicial);
+        //Login login = new Login();
+        //new ControlerLogin(login);
         
     }
     
